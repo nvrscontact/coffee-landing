52d9bf911c6b36b33ff4479b58e4b198a038da2f
nvrsunk <nvrsdark@gmail.com>
2025-06-15 15:32:38 +0200

    g

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,31 +4,30 @@ function NavBar() {
       const date = new Date().getFullYear()
   return (
     <>
-    <section className='flex flex-col gap-8 text-[14px] border-t border-white/30'>
+    <div className='flex flex-col gap-8 text-[13px] border-t border-white/30 fira-sans-extralight'>
+      <section className='ml-5 my-6'>
 
-    <section className='ml-5 my-8'>
+        <section className='flex mb-7'>
+            <li className='flex gap-3 items-center'>
+                <a href="https://facebook.com"><img src="/images/instagram.svg" alt="" className='h-5' /></a>
+                <a href="https://facebook.com"><img src="/images/tik-tok.svg" alt="" className='h-4' /></a>
+                <a href="https://facebook.com"><img src="/images/x.svg" alt="" className='h-5' /></a>
+            </li>
+        </section>
 
-    <section className='flex mb-7'>
-        <li className='flex gap-3 items-center'>
-            <a href="https://facebook.com"><img src="/images/instagram.svg" alt="" className='h-5' /></a>
-            <a href="https://facebook.com"><img src="/images/tik-tok.svg" alt="" className='h-4' /></a>
-            <a href="https://facebook.com"><img src="/images/x.svg" alt="" className='h-5' /></a>
-        </li>
-    </section>
-
-     <section className='flex flex-col gap-3 mb-7 text-white/50 cormorant-400'>
-        <a className='hover-white' href='https://www.apple.com'>Contact</a>
-        <a className='hover-white' href='https://www.apple.com'>About Us</a>
-        <a className='hover-white' href='https://www.apple.com'>Work with us</a>
-        <a className='hover-white' href='https://www.apple.com'>Help & Support</a>
-        <a className='hover-white' href='https://www.apple.com'>Privacy & Cookies</a>
+        <section className='flex flex-col gap-3 mb-7 text-white/50 '>
+            <a className='hover-white' href='https://www.apple.com'>Contact</a>
+            <a className='hover-white' href='https://www.apple.com'>About Us</a>
+            <a className='hover-white' href='https://www.apple.com'>Work with us</a>
+            <a className='hover-white' href='https://www.apple.com'>Help & Support</a>
+            <a className='hover-white' href='https://www.apple.com'>Privacy & Cookies</a>
+        </section>
+        <span className='flex flex-col text-white/30  text-[12px] '>Ruseau's Coffe © {date}
+          <span className='colrs'>All rights reserved.</span>
+        </span>
+      
       </section>
-      <span className='flex flex-col text-white/30 text-[12px] cormorant-400 '>Ruseau's Coffe © {date}
-        <span>All rights reserved.</span>
-      </span>
-    
-    </section>
-    </section>
+    </div>
     </>
   )
 }
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Reviews from '../Components/Reviews'
 
 
-import Vertical from '../assets/vertical-image.png'
+import CoffeeShop from '../assets/coffee_shop.jpg'
 import OneFood from '../assets/one-food.jpg'
 import TwoFood from '../assets/two-food.jpg'
 import ThreeFood from '../assets/three-food.jpg'
@@ -35,10 +35,9 @@ function HomePage() {
   return (
       <>
 
-      <section className='flex gap-6 m-5 pb-4'>
+      <div className='flex gap-6 ml-5 pb-4'>
 
-          <section className='w-40 flex flex-col gap-2 pt-5'>
-          
+          <div className='w-40 flex flex-col gap-2 pt-5'>
             <section className='flex flex-col'>
               <h1 className='text-[20px] text-[#cea65bc7] fira-sans-extralight'>Rosetti's Coffee</h1>
               <p className='text-[11px] text-white/60 '>
@@ -47,52 +46,48 @@ function HomePage() {
               </p>
             </section>
 
-          <section>
+            <section>
+              <p className='flex gap-2 text-[12px] text-white/50 border-t border-white/20 py-3 cormorant-400'>
+              <span><img src={Wine} alt="" className='h-4' /></span>
+              <span><img src={Coffe} alt="" className='h-4' /></span>
+              <span><img src={Spoon} alt="" className='h-4' /></span>
+              <span><img src={Wifi} alt="" className='h-4' /></span>
+              </p>
 
-            <p className='flex gap-2 text-[12px] text-white/50 border-t border-white/20 py-3 cormorant-400'>
-            <span><img src={Wine} alt="" className='h-4' /></span>
-            <span><img src={Coffe} alt="" className='h-4' /></span>
-            <span><img src={Spoon} alt="" className='h-4' /></span>
-            <span><img src={Wifi} alt="" className='h-4' /></span>
-            </p>
-
-            <p className='flex flex-col gap-1 text-[10px] text-white/30 '>
-              <span>Do you work here.</span>
-              <span>Variety of drinks and wines </span>
-              <span>Good ilmumination and concentration </span>
-            </p>
-
-          </section>
-
-          </section>
-            
-            <section className='flex flex-col '>
-              <section className='relative mb-2 items-center'>
-                  <img src={Vertical} alt="" className='w-45 rounded-xl' />
-                  <p className='absolute bottom-2 left-6'>
-                  <button className='px-4 p-2 flex gap-1 items-center text-[10px] text-[#c7c7c7] border border-white/30 rounded-xl backdrop-blur-xl'><img src={Schedule} alt="" className='h-[10px] ' /> Make Reservation</button>
-                  </p>
-              </section>
-                <span className='text-[9px] text-center ml-2'><span>{message}</span></span>
+              <p className='flex flex-col gap-1 text-[10px] text-white/30 '>
+                <span>Do you work here.</span>
+                <span>Variety of drinks and wines </span>
+                <span>Good ilmumination and concentration </span>
+              </p>
             </section>
-       </section>  
+          </div>
+            
+            <section className='flex flex-col gap-2 text-[10px] text-white/60 '>
+                <img src={CoffeeShop} alt="" className='w-48 h-52 rounded-xl' />
+                <section className='flex items-center gap-2'>
+                  <button className='p-2 gap-1 flex items-center border border-white/30 rounded-xl w-fit'><img src={Schedule} alt="" className='h-[10px] '/> Make Reservation</button>
+                  <span className='text-[9px] text-center'><span>{message}</span></span>
+                </section>  
+            </section>
 
-        <section className='flex flex-col gap-10 mb-20'>
-          <FoodImages img={OneFood} title={'Katerin'} paragraph={'the best food are here, and you will want to eat!'}/>
-          <FoodImages img={TwoFood} title={'The Kuznetsova Bar'} paragraph={'the best food are here, and you will want to eat!'}/>
-          <FoodImages img={ThreeFood} title={'Katerin'} paragraph={'the best food are here, and you will want to eat!'}/>
-        </section>
+       </div>  
+
+      <div className='flex flex-col gap-10 mb-20'>
+        <FoodImages img={OneFood} title={'Katerin'} paragraph={'the best food are here, and you will want to eat!'}/>
+        <FoodImages img={TwoFood} title={'The Kuznetsova Bar'} paragraph={'the best food are here, and you will want to eat!'}/>
+        <FoodImages img={ThreeFood} title={'Katerin'} paragraph={'the best food are here, and you will want to eat!'}/>
+      </div>
 
 
       
-      <section className='mx-5 mb-10'>
+      <div className='mx-5 mb-10'>
         <h1 className='mb-4 text-white/50 text-[20px] cormorant-400'>What some people say...</h1>
         <section className='flex flex-col gap-4'>
           <Reviews names='Katterin R.' dates='December 2024' paragraph='Excellent food.' />
           <Reviews names='Mascherandossk' dates='Summer of 2025' paragraph='Excellent food and sprites, during summer is great.' />
           <Reviews names='Jennifer' dates='April 2023' paragraph='Its unique.' />
         </section>
-      </section>
+      </div>
 
       </>
   )
