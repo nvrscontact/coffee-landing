804c5c48594e0ebb90c2234f1ff5896df1961b01
nvrsunk <nvrsdark@gmail.com>
2025-07-25 12:32:33 +0200

    g

--- src/Components/Presentation.jsx
+++ src/Components/Presentation.jsx
@@ -6,6 +6,13 @@ import Wifi from '../assets/wifi.svg'
 import Wine from '../assets/wine.svg'
 import Spoon from '../assets/spoon.svg'
 import Hotelbg from '../assets/hotelbg.jpg'
+import ParkingGray from '../assets/parking_gray.svg'
+import PoolGray from '../assets/pool_gray.svg'
+import WifiGray from '../assets/wifi_gray.svg'
+import BarGray from '../assets/bar_gray.svg'
+import GymGray from '../assets/gym_gray.svg'
+import SpaGray from '../assets/spa_gray.svg'
+
 import {Link} from 'react-router-dom'
 
 
@@ -24,7 +31,7 @@ function Presentation() {
       <div className='flex flex-col items-center justify-center mt-10 fira-sans-thin '>
 
               <section className='flex flex-col '>
-                  <h1 className='text-3xl text-[#7a520d] inter-300'>Rosetti's Hotel</h1>
+                  <h1 className='text-3xl text-[#7a520d] fira-sans-extralight'>Rosetti's Hotel</h1>
                     <p className='text-[12px] flex flex-col items-center mt-1 text-white/60 text-center '>
                       <span className='inter-200'>Calle Na Saurina de Entenza 60.</span>
                       <span className='inter-200 flex itmes-center gap-1 '>Alcoy, Alicante</span>
@@ -34,26 +41,57 @@ function Presentation() {
               <section className='flex gap-2 mt-3 text-white/70 inter-200'>
 
                 <div className=' flex flex-col items-center gap-3 '>
-                  <h1 className='text-[12px] bg-[#3b2808] py-1 px-2 rounded-md'>Bar</h1>
+                  <h1 className='text-[12px] text-[#946817] border border-[#2b1e05] py-1 px-2 rounded-[4px] '>Resort</h1>
                 </div>
 
                 <div className=' flex flex-col items-center gap-3 '>
-                  <h1 className='text-[12px] bg-[#3b2808] py-1 px-2 rounded-md'>Restaurant</h1>
+                  <h1 className='text-[12px] text-[#946817] border border-[#2b1e05] py-1 px-2 rounded-[4px]'>Hosting</h1>
                 </div>
 
                 <div className=' flex flex-col items-center gap-3 '>
-                  <h1 className='text-[12px] bg-[#3b2808] py-1 px-2 rounded-md'>Hosting</h1>
+                  <h1 className='text-[12px] text-[#946817] border border-[#2b1e05] py-1 px-2 rounded-[4px]'>Restaurant</h1>
                 </div>
                 
               </section>
 
-              <section className='relative'>
-              <img src={Hotelbg} alt="" className='mt-10 opacity-40 ' />
-                <div className='absolute bottom-4 left-4 flex flex-col gap-1 fira-sans-thin text-[12px] text-white/80 '>
-                  <span className=''>Mejorando cada</span>
-                  <span className=''>Haciendolo mejor cada </span>
-                  <span className=''>Haciendolo y mejorando mas</span>
+              <section className='flex flex-col justify-center items-center'>
+                <img src={Hotelbg} alt="" className='mt-10 opacity-40 ' />
+
+
+                <div className='mt-4 flex gap-2 text-white/70'>
+                  
+                  <section className='flex items-center gap-2 w-fit border border-white/20 py-1 px-2 rounded-md '>
+                    <img src={ParkingGray} alt="" className='h-4' />
+                    <span className='text-[12px] '>Parking Free</span>
+                  </section>
+                  <section className='flex items-center gap-2 w-fit border border-white/20 py-1 px-2 rounded-md '>
+                    <img src={PoolGray} alt="" className='h-4' />
+                    <span className='text-[12px] '>Pool</span>
+                  </section>
+                  <section className='flex items-center gap-2 w-fit border border-white/20 py-1 px-2 rounded-md '>
+                    <img src={WifiGray} alt="" className='h-4' />
+                    <span className='text-[12px] '>Wifi Free 80 Mbps </span>
+                  </section>
                 </div>
+
+                <div className='mt-2 flex gap-2 text-white/70'>
+                
+                  <section className='flex items-center gap-2 w-fit border border-white/20 py-1 px-2 rounded-md '>
+                    <img src={BarGray} alt="" className='h-4' />
+                    <span className='text-[12px] '>Bar</span>
+                  </section>
+        
+                  <section className='flex items-center gap-2 w-fit border border-white/20 py-1 px-2 rounded-md '>
+                    <img src={SpaGray} alt="" className='h-4' />
+                    <span className='text-[12px] '>Spa & Sauna </span>
+                  </section>
+                  <section className='flex items-center gap-2 w-fit border border-white/20 py-1 px-2 rounded-md '>
+                    <img src={GymGray} alt="" className='h-4' />
+                    <span className='text-[12px] '>Gym </span>
+                  </section>
+                </div>
+
+
               </section>
 
         </div>  
