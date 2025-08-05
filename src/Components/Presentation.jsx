f254199fb8ea9e803afcbbc8b5c0d8fba0d3b6e0
nvrsunk <nvrsdark@gmail.com>
2025-08-05 23:18:53 +0200

    g

--- src/Components/Presentation.jsx
+++ src/Components/Presentation.jsx
@@ -31,15 +31,15 @@ function Presentation() {
 
   return (
   <>
-              <section className='relative w-full h-screen overflow-hidden'>
+              <section className='relative w-full h-[45rem] overflow-hidden'>
                 <video  className='relative object-cover w-full h-screen ' autoPlay muted loop playsInline>
                   <source src={hotel_video} type='video/mp4' />
                 </video>
 
-                  {/* <div className="absolute top-0 left-0 w-full mt-10">
+                   <div className="absolute top-0 left-0 w-full mt-10 text-white/70">
                       <section className='flex flex-col items-start py-3 ml-6 '>
-                          <h1 className='text-4xl text-white inter-100'>Rosetti's Hotel</h1>
-                            <p className='text-[12px] flex flex-col space-y-1 items-start text-white text-center '>
+                          <h1 className='text-4xl inter-100'>Rosetti's Hotel</h1>
+                            <p className='text-[12px] flex flex-col space-y-1 items-start text-center '>
                                 <span className='inter-200 flex itmes-center gap-1 text-[16px] '>Benidorm, Alicante</span>
                                   <section className='flex gap-1 items-center'>
                                     <div className='flex [&>*]:h-[18px] -space-x-0.5 '>
@@ -49,23 +49,23 @@ function Presentation() {
                                       <img src={Star} alt="" />
                                       <img src={Star} alt="" />
                                     </div>
-                                    <span className='text-[10px] text-white inter-300 '>4,5</span>
+                                    <span className='text-[10px] inter-300 '>4,5</span>
                                   </section>
                             </p>
                         </section>
                   </div>
 
-                  <div className='absolute inter-200 text-white bottom-6 right-6 space-y-3 flex flex-col justify-end items-end w-full glow-white'>
-                    <h3 className='text-2xl flex items-center gap-3'>Hotel <img src={arrow} alt="" className='h-4 drop-shadow-[0_0_1px_rgb(255,255,255),0_0_0.5px_rgb(255,255,255)]' /></h3>
-                    <h3 className='text-2xl flex items-center gap-3 pr-2 '>Resort <img src={arrow} alt="" className='h-4 drop-shadow-[0_0_1px_rgb(255,255,255),0_0_0.5px_rgb(255,255,255)]' /></h3>
-                    <h3 className='text-2xl flex items-center gap-3 pr-4'>Restaurant <img src={arrow} alt="" className='h-4 drop-shadow-[0_0_1px_rgb(255,255,255),0_0_0.5px_rgb(255,255,255)]' /></h3>
-                  </div> */}
+                  <div className='absolute inter-200 text-white/70 bottom-10 right-6 space-y-3 flex flex-col justify-end items-end w-full'>
+                    <h3 className='text-2xl flex items-center gap-3'>Hotel <img src={arrow} alt="" className='h-4' /></h3>
+                    <h3 className='text-2xl flex items-center gap-3 pr-2 '>Resort <img src={arrow} alt="" className='h-4' /></h3>
+                    <h3 className='text-2xl flex items-center gap-3 pr-4'>Restaurant <img src={arrow} alt="" className='h-4' /></h3>
+                  </div>
 
               </section>
 
-{/*         <div className='bg-black/90 text-white text-[10px] py-2 text-center inter-300 mb-20'>
+         <div className='bg-black/90 text-white text-[10px] py-2 text-center inter-300 mb-20'>
           <span className=''>10% discount on rooms for two persons</span>
-        </div> */}
+        </div>
     </>
   )
 }
