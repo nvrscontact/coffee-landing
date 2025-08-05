a15665f22e8394c913978bd3baa001fe8bff3635
nvrsunk <nvrsdark@gmail.com>
2025-08-05 23:13:25 +0200

    g

--- src/Components/Presentation.jsx
+++ src/Components/Presentation.jsx
@@ -31,13 +31,12 @@ function Presentation() {
 
   return (
   <>
-      <div className=''>
-              <section className='relative w-full h-[37rem] overflow-hidden'>
-                <video  className='absolute object-cover w-full h-screen ' autoPlay muted loop playsInline>
+              <section className='relative w-full h-screen overflow-hidden'>
+                <video  className='relative object-cover w-full h-screen ' autoPlay muted loop playsInline>
                   <source src={hotel_video} type='video/mp4' />
                 </video>
 
-                  <div className="absolute top-0 left-0 w-full mt-10">
+                  {/* <div className="absolute top-0 left-0 w-full mt-10">
                       <section className='flex flex-col items-start py-3 ml-6 '>
                           <h1 className='text-4xl text-white inter-100'>Rosetti's Hotel</h1>
                             <p className='text-[12px] flex flex-col space-y-1 items-start text-white text-center '>
@@ -60,14 +59,13 @@ function Presentation() {
                     <h3 className='text-2xl flex items-center gap-3'>Hotel <img src={arrow} alt="" className='h-4 drop-shadow-[0_0_1px_rgb(255,255,255),0_0_0.5px_rgb(255,255,255)]' /></h3>
                     <h3 className='text-2xl flex items-center gap-3 pr-2 '>Resort <img src={arrow} alt="" className='h-4 drop-shadow-[0_0_1px_rgb(255,255,255),0_0_0.5px_rgb(255,255,255)]' /></h3>
                     <h3 className='text-2xl flex items-center gap-3 pr-4'>Restaurant <img src={arrow} alt="" className='h-4 drop-shadow-[0_0_1px_rgb(255,255,255),0_0_0.5px_rgb(255,255,255)]' /></h3>
-                  </div>
+                  </div> */}
 
               </section>
 
-        </div>  
-        <div className='bg-black/90 text-white text-[10px] py-2 text-center inter-300 mb-20'>
+{/*         <div className='bg-black/90 text-white text-[10px] py-2 text-center inter-300 mb-20'>
           <span className=''>10% discount on rooms for two persons</span>
-        </div>
+        </div> */}
     </>
   )
 }
